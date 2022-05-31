a98d3ace6f5ec2607411d770a7fe1b636108a378
igoryan <nkulinigor1984@icloud.com>
2022-05-31 22:17:56 +0200

    task_05

---
+++ task_05/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю 
+// строку массива.
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+int[,] ReplaceRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int value = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = value;
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = 5;
+int col = 10;
+int min = 1;
+int max = 10;
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] matrix2 = ReplaceRow(matrix);
+PrintMatrix(matrix2);
